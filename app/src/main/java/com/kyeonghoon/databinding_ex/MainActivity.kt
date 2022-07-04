6039dceab5c73bf61a5198ac23c29fc0a4822983
zhaqj12 <zhaqj12@naver.com>
2022-07-04 11:50:15 +0900

    If 노가다, Dice App Make

--- app/src/main/java/com/kyeonghoon/databinding_ex/MainActivity.kt
+++ app/src/main/java/com/kyeonghoon/databinding_ex/MainActivity.kt
@@ -6,29 +6,66 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.kyeonghoon.databinding_ex.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-   private lateinit var binding : ActivityMainBinding
+private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val diceImage1 = binding.dice1
+        val diceImage2 = binding.dice2
+        binding.diceStartBtn.setOnClickListener {
+            Toast.makeText(this, "주사위 GO", Toast.LENGTH_SHORT).show()
 
-        // 1번 방식 - findViewById
-        //val btn = findViewById<Button>(R.id.testBtmId)
-        //btn.setOnClickListener {
-            //Toast.makeText(this,"Click",Toast.LENGTH_LONG).show()
-        //}
+            val number1 = Random.nextInt(1,6)
+            val number2 = Random.nextInt(1,6)
 
-//        //2번 방식 Databinding
-       binding.testBtmId.setOnClickListener{
-           Toast.makeText(this,"CK",Toast.LENGTH_LONG).show()
+            if(number1 == 1)
+            {
+                diceImage1.setImageResource(R.drawable.dice_1)
+            }
+            else if(number1 == 2){
+                diceImage1.setImageResource(R.drawable.dice_2)
+            }
+            else if(number1 == 3){
+                diceImage1.setImageResource(R.drawable.dice_3)
+            }
+            else if(number1 == 4){
+                diceImage1.setImageResource(R.drawable.dice_4)
+            }
+            else if(number1 == 5){
+                diceImage1.setImageResource(R.drawable.dice_5)
+            }
+            else if(number1 == 6){
+                diceImage1.setImageResource(R.drawable.dice_6)
+            }
+
+            if(number2 == 1)
+            {
+                diceImage2.setImageResource(R.drawable.dice_1)
+            }
+            else if(number2 == 2){
+                diceImage2.setImageResource(R.drawable.dice_2)
+            }
+            else if(number2 == 3){
+                diceImage2.setImageResource(R.drawable.dice_3)
+            }
+            else if(number2 == 4){
+                diceImage2.setImageResource(R.drawable.dice_4)
+            }
+            else if(number2 == 5){
+                diceImage2.setImageResource(R.drawable.dice_5)
+            }
+            else if(number2 == 6){
+                diceImage2.setImageResource(R.drawable.dice_6)
+            }
+
         }
-
-
     }
 
 
