4c050fce0439c954516aa28d98cb6501cee8390b
zhaqj12 <zhaqj12@naver.com>
2022-07-04 09:56:24 +0900

    Version Update

--- app/src/main/java/com/kyeonghoon/databinding_ex/MainActivity.kt
+++ app/src/main/java/com/kyeonghoon/databinding_ex/MainActivity.kt
@@ -4,20 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import com.kyeonghoon.databinding_ex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+//    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         // 1번 방식 - findViewById
-        val btn = findViewById<Button>(R.id.testBtmId)
-        btn.setOnClickListener {
-            Toast.makeText(this,"Click",Toast.LENGTH_LONG).show()
-        }
+        //val btn = findViewById<Button>(R.id.testBtmId)
+        //btn.setOnClickListener {
+            //Toast.makeText(this,"Click",Toast.LENGTH_LONG).show()
+        //}
 
-        //2번 방식 Databinding
+//        //2번 방식 Databinding
+//        binding.testBtmId.setOnClickListener{
+//            Toast.makeText(this,"CK",Toast.LENGTH_LONG).show()
+//        }
+
+
     }
 
 
